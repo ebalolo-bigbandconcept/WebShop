b102d28d933ac14373db64dd98071313c98f22ec
AnakinGig <gigantanakin19@gmail.com>
2026-03-19 11:07:32 +0100

    Refactor code structure for improved readability and maintainability

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import httpClient from "../components/httpClient";
 import { useToast } from "../components/Toast";
+import Webshop from "../assets/Webshop.jpg";
 
 function Login({ setUser }) {
   const navigate = useNavigate();
@@ -71,7 +72,7 @@ function Login({ setUser }) {
     <div className="vh-100 d-flex justify-content-center align-items-center">
       <div className="row d-flex justify-content-center align-items-center h-100">
         <div className="col-md-9 col-lg-6 col-xl-5">
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sample"/>
+          <img src={Webshop} className="img-fluid" alt="Webshop Logo"/>
         </div>
         <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
           <form>
